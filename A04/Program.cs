30f4512a199409251e0a7a2973f61f7f1f4f2a31
Deepakhannan Lakshmipathi <Deepakhannan.Lakshmipathi@trumpf.com>
2025-11-19 15:28:45 +0530

    A04: EXTENSION OF SPELLING BEE ASSIGNMENT (#3)
    * Frequency Table
    
    * Review fix 1
    
    * Review fix 2
    
    * Review fix 3

---
+++ A04/Program.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to build of frequency table of letters in a file
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+namespace A04;
+
+#region class Program------------------------------------------------------------------------------
+class Program {
+   static void Main () {
+      var charFreq = File.ReadAllText ("words.txt")
+                         .Where (char.IsLetter)
+                         .GroupBy (ch => ch)
+                         .Select (g => new { Letter = g.Key, Count = g.Count () })
+                         .OrderByDescending (g => g.Count)
+                         .Take(7);
+      WriteLine ($"The 7 most recurring letters are: ");
+      foreach (var pair in charFreq) WriteLine ($"{pair.Letter} - {pair.Count}");
+   }
+}
+#endregion
