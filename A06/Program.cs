00c512515d71086a0845b80b5e701043c8a66521
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-12-18 11:33:11 +0530

    Review fix 1

--- A06/Program.cs
+++ A06/Program.cs
@@ -11,105 +11,101 @@ using static System.Console;
 #region class Program -----------------------------------------------------------------------------
 class Program {
    static void Main () {
-      OutputEncoding = new UnicodeEncoding ();
+      OutputEncoding = Encoding.UTF8;
       QueenPos (0);
-      PrintAllSoln (solutions);
+      PrintAllSoln (sSolutions);
    }
 
    #region Implementation -------------------------------------------
    // Finds all the unique solutions and stores them in an array
    static void QueenPos (int row) {
-      if (row == 8) {
-         int[] solution = (int[])tempSoln.Clone ();
-         if (PrevCheck (solution)) solutions[count++] = solution;
+      if (row is 8) {
+         int[] solution = (int[])sTempSoln.Clone ();
+         if (PrevCheck (solution)) sSolutions[sCount++] = solution;
          return;
       }
-      for (int column = 0; column < 8; column++) {
-         if (isValid (row, column)) {
-            tempSoln[row] = column;
+      for (int i = 0; i < sFreeColumns.Count; i++) {
+         int column = sFreeColumns[i];
+         if (IsValidPos (row, column)) {
+            sTempSoln[row] = column;
+            sFreeColumns.RemoveAt (i);
             QueenPos (row + 1);
+            sFreeColumns.Insert (i, column);
          }
       }
    }
 
    // Checks if the input position is a valid position
-   static bool isValid (int row, int column) {
+   static bool IsValidPos (int row, int column) {
+      int prevColumn;
       for (int prevRow = 0; prevRow < row; prevRow++) {
-         int prevColumn = tempSoln[prevRow];
-         if (prevColumn == column || Math.Abs (prevRow - row) == Math.Abs (prevColumn - column))
+         prevColumn = sTempSoln[prevRow];
+         if (Math.Abs (prevRow - row) == Math.Abs (prevColumn - column))
             return false;
       }
       return true;
    }
 
-   // Rotates the given array by 90 degrees clockwise
-   static int[] Rotate90 (int[] arr) {
-      int[] array90 = new int[8];
-      for (int row = 0; row < 8; row++) array90[arr[row]] = 7 - row;
-      return array90;
-   }
-
-   // Rotates the given array by 180 degrees clockwise
-   static int[] Rotate180 (int[] arr) => Rotate90 (Rotate90 (arr));
-
-   // Rotates the given array by 270 degrees clockwise
-   static int[] Rotate270 (int[] arr) => Rotate90 (Rotate180 (arr));
-
-   // Mirrors the given array along the horizontal axis
-   static int[] Mirror (int[] arr) {
-      int[] mirror = new int[8];
-      for (int r = 0; r < 8; r++) mirror[r] = 7 - arr[r];
-      return mirror;
-   }
-
-   // Checks if given 2 arrays are duplicates of each other
-   static bool IsDuplicate (int[] arr1, int[] arr2) {
-      for (int i = 0; i < 8; i++) if (arr1[i] != arr2[i]) return false;
-      return true;
-   }
-
    // Checks if the found solution already exists in other forms
    static bool PrevCheck (int[] sol) {
-      Func<int[], int[]>[] tests = [ Rotate90, Rotate180, Rotate270, Mirror,
-                                     arr => Mirror (Rotate90 (arr)),
-                                     arr => Mirror (Rotate180 (arr)),
-                                     arr => Mirror (Rotate270 (arr)) ];
-      for (int i = 0; i < count; i++) {
-         var c = solutions[i];
-         foreach (var check in tests) if (IsDuplicate (check (sol), c)) return false;
-      }
+      for (int i = 0; i < sCount; i++)
+         foreach (var check in Tests (sol)) if (check.SequenceEqual (sSolutions[i])) return false;
       return true;
+
+      // Returns the various transformations of an arr to check duplicates
+      static int[][] Tests (int[] arr) {
+         // Rotated arrays by 90, 180 and 270 degrees
+         int[] arr90 = Rotate (arr, 1), arr180 = Rotate (arr, 2), arr270 = Rotate (arr, 3);
+         // Mirror along the horizontal axis is same as vertical mirror rotated 180 degrees
+         return [arr90, arr180, arr270, Mirror (arr),
+                 Mirror (arr90), Mirror (arr180), Mirror (arr270)];
+
+         // Rotates the given array by 90 degrees clockwise by given number of times
+         static int[] Rotate (int[] arr, int count) {
+            int[] rotArr = (int[])arr.Clone ();
+            for (int i = 0; i < count; i++) {
+               int[] tempArr = new int[8];
+               for (int row = 0; row < 8; row++) tempArr[rotArr[row]] = 7 - row;
+               rotArr = tempArr;
+            }
+            return rotArr;
+         }
+
+         // Mirrors the given array along the vertical axis
+         static int[] Mirror (int[] arr) {
+            int[] mirror = new int[8];
+            for (int r = 0; r < 8; r++) mirror[r] = 7 - arr[r];
+            return mirror;
+         }
+      }
    }
 
-   // Prints the solution in a chess board
+   // Prints all the solution in a chess board
    static void PrintAllSoln (int[][] sol) {
-      for (int i = 0; i < count; i++) {
+      for (int i = 0; i < sCount; i++) {
          WriteLine ($"Solution {i + 1}:");
          PrintSoln (sol[i]);
          WriteLine ();
       }
 
+      // Prints one of the solution in a chess board
       static void PrintSoln (int[] sol) {
-         string empLine = "│       │       │       │       │       │       │       │       │";
-         WriteLine ("┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐");
+         WriteLine ("┌───┬───┬───┬───┬───┬───┬───┬───┐");
          for (int r = 0; r < 8; r++) {
-            WriteLine (empLine);
-            for (int i = 0; i < sol[r]; i++) Write ("│       ");
-            Write ($"│   ♛   ");
-            for (int i = 0; i < 7 - sol[r]; i++) Write ("│       ");
-            WriteLine ($"│\n{empLine}");
-            WriteLine (r is 7
-                       ? "└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘"
-                       : "├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤");
+            for (int i = 0; i < 8; i++) Write ($"│ {((i == sol[r]) ? '♛' : ' ')} ");
+            WriteLine ($"│\n{(r is 7
+                              ? "└───┴───┴───┴───┴───┴───┴───┴───┘"
+                              : "├───┼───┼───┼───┼───┼───┼───┼───┤")}");
          }
       }
    }
    #endregion
 
    #region Fields ---------------------------------------------------
-   static int[] tempSoln = new int[8];
-   static int[][] solutions = new int[12][];
-   static int count = 0;
+   static int[] sTempSoln = new int[8];
+   static int[][] sSolutions = new int[12][];
+   static int sCount = 0;
+   static List<int> sFreeColumns = [0, 1, 2, 3, 4, 5, 6, 7];
    #endregion
 }
 #endregion
