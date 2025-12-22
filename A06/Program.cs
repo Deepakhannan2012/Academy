11cbb51ff419ff77ed28491998f9d9fee3724709
Deepakhannan <deepakhannan.lakshmipathi@trumpf.com>
2025-12-22 15:41:52 +0530

    Review fix 3

--- A06/Program.cs
+++ A06/Program.cs
@@ -3,15 +3,15 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program to find the 8 Queens solution
+// Program to find the 12 unique solutions for 8 queens
 // ------------------------------------------------------------------------------------------------
-using System.Text;
 using static System.Console;
 
 #region class Program -----------------------------------------------------------------------------
 class Program {
    static void Main () {
-      OutputEncoding = Encoding.UTF8;
+      OutputEncoding = System.Text.Encoding.UTF8;
+      // The first queen is checked at row 0 and column 0
       QueenPos (0);
       PrintAllSoln (sUniqueSolns);
    }
@@ -19,16 +19,17 @@ class Program {
    #region Implementation -------------------------------------------
    // Checks if the found solution already exists
    static bool DuplicateCheck (int[] sol) {
-      var chkArr = Tests (sol);
+      var alterSoln = TransformSoln (sol);
       for (int i = 0; i < sCount; i++)
-         foreach (var check in chkArr) if (check.SequenceEqual (sUniqueSolns[i])) return false;
+         foreach (var soln in alterSoln) if (soln.SequenceEqual (sUniqueSolns[i])) return false;
       return true;
 
       // Returns the various transformations of an arr to check duplicates
-      static int[][] Tests (int[] arr) {
+      static int[][] TransformSoln (int[] arr) {
          // Rotated arrays by 90, 180 and 270 degrees
          int[] arr90 = Rotate (arr, 1), arr180 = Rotate (arr, 2), arr270 = Rotate (arr, 3);
-         // Vertical and horizontal mirrors are equivalent with a 180° phase shift.
+         // Vertical mirror is equivalent with horizontal mirrors when they have a
+         // difference of 180°
          // Example: vertical at 0° is same as horizontal at 180°, vertical at 90° is same
          // as horizontal at 270°
          return [arr90, arr180, arr270, Mirror (arr),
@@ -36,17 +37,17 @@ class Program {
 
          // Mirrors the given array along the vertical axis
          static int[] Mirror (int[] arr) {
-            int[] mirror = new int[8];
-            for (int r = 0; r < 8; r++) mirror[r] = 7 - arr[r];
+            int[] mirror = new int[MAXROW];
+            for (int r = 0; r < MAXROW; r++) mirror[r] = MAXROW - 1 - arr[r];
             return mirror;
          }
 
-         // Rotates the given array by 90 degrees clockwise by given number of times
+         // Rotates the given array by 90° clockwise by given number of times
          static int[] Rotate (int[] arr, int count) {
-            int[] tempArr = new int[8];
+            int[] tempArr = new int[MAXROW];
             for (int i = 0; i < count; i++) {
-               Array.Clear (tempArr);
-               for (int row = 0; row < 8; row++) tempArr[arr[row]] = 7 - row;
+               if (i is not 0) Array.Clear (tempArr);
+               for (int row = 0; row < MAXROW; row++) tempArr[arr[row]] = MAXROW - 1 - row;
                arr = (int[])tempArr.Clone ();
             }
             return arr;
@@ -57,15 +58,15 @@ class Program {
    // Checks if the column position is valid
    static bool IsValidPos (int row, int column) {
       for (int prevRow = 0; prevRow < row; prevRow++) {
-         // 2 positions lie on the same diagonal if the absolute difference of row numbers is
-         // equal to the absolute difference of the column numbers
+         // The previous queen is considered as the origin for current queen position
+         // to check the diagonal
          if (Math.Abs (prevRow - row) == Math.Abs (sTempSoln[prevRow] - column))
             return false;
       }
       return true;
    }
 
-   // Prints all the solution in a chess board
+   // Prints all the solutions in a chess board
    static void PrintAllSoln (int[][] sol) {
       for (int i = 0; i < sCount; i++) {
          WriteLine ($"Solution {i + 1}:");
@@ -75,23 +76,23 @@ class Program {
       // Prints one of the solution in a chess board
       static void PrintSoln (int[] sol) {
          WriteLine ("┌───┬───┬───┬───┬───┬───┬───┬───┐");
-         for (int r = 0; r < 8; r++) {
-            for (int i = 0; i < 8; i++) Write ($"│ {((i == sol[r]) ? '♛' : ' ')} ");
-            WriteLine ($"│\n{(r is 7 ? "└───┴───┴───┴───┴───┴───┴───┴───┘\n"
-                                     : "├───┼───┼───┼───┼───┼───┼───┼───┤")}");
+         for (int r = 0; r < MAXROW; r++) {
+            for (int i = 0; i < MAXROW; i++) Write ($"│ {((i == sol[r]) ? '♛' : ' ')} ");
+            WriteLine ($"│\n{(r is (MAXROW - 1) ? "└───┴───┴───┴───┴───┴───┴───┴───┘\n"
+                                                : "├───┼───┼───┼───┼───┼───┼───┼───┤")}");
          }
       }
    }
 
    // Finds all the unique solutions and stores them in an array
    static void QueenPos (int row) {
-      if (row is mMaxRow) {
+      if (row is MAXROW) {
          int[] solution = (int[])sTempSoln.Clone ();
          if (DuplicateCheck (solution)) sUniqueSolns[sCount++] = solution;
          return;
       }
-      int availCount = sAvailableColPos.Count;
-      for (int i = 0, column; i < availCount; i++) {
+      int column, availCount = sAvailableColPos.Count;
+      for (int i = 0; i < availCount; i++) {
          column = sAvailableColPos[i];
          if (IsValidPos (row, column)) {
             sTempSoln[row] = column;
@@ -108,12 +109,12 @@ class Program {
    static List<int> sAvailableColPos = [0, 1, 2, 3, 4, 5, 6, 7];
    // Counts the number of unique solutions found
    static int sCount = 0;
-   // Stores the constant value 8
-   const int mMaxRow = 8;
    // Stores the temporary solution until a unique solution is found
-   static int[] sTempSoln = new int[8];
-   // Stores every unique solution found
+   static int[] sTempSoln = new int[MAXROW];
+   // 12 indicates the unique solutions that are found for 8 queens
    static int[][] sUniqueSolns = new int[12][];
+   // 8 indicates the maximum rows in a chess board
+   const int MAXROW = 8;
    #endregion
 }
 #endregion
